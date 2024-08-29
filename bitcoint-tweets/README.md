f1e8c68883045106c98a29fed4dd64c6bed16103
boa50 <bruno.albuquerque50@gmail.com>
2024-08-29 11:43:18 -0300

    Organised the bitcoins tweets and prices datasets

--- bitcoint-tweets/README.md
+++ bitcoint-tweets/README.md
@@ -2,3 +2,4 @@ Talking about Bitcoin popularity on twitter
 
 Data source:
  - Bitcoin_tweets.csv[https://www.kaggle.com/datasets/kaushiksuresh147/bitcoin-tweets]
+ - BTC-USD.csv[https://finance.yahoo.com/quote/BTC-USD/history/?period1=1612137600&period2=1675123200]
--- bitcoint-tweets/data/etl.py
+++ bitcoint-tweets/data/etl.py
@@ -6,6 +6,7 @@ def get_path(file_name):
     return path.join(path.dirname(path.realpath(__file__)), file_name)
 
 
+### tweets
 df = pd.read_csv(
     get_path("Bitcoin_tweets.csv"),
     usecols=["date"],
@@ -16,8 +17,19 @@ df = pd.read_csv(
 
 df["date"] = df["date"].str.slice(0, 10)
 df = df["date"].value_counts().reset_index()
+# To remove non date values
 df = df[df["count"] >= 10]
 
 print(df.tail())
 
-df.to_csv(get_path("dataset.csv"), index=False)
+df.to_csv(get_path("tweets.csv"), index=False)
+
+### prices
+df = pd.read_csv(get_path("BTC-USD.csv"), usecols=["Date", "Close"])
+
+df.columns = ["date", "close"]
+
+print(df.head())
+print(df.isna().sum())
+
+df.to_csv(get_path("prices.csv"), index=False)
